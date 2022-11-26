7393aace2c4c7de71fa62fa4ca46147353557069
basavaraj <bdiddege@gmail.com>
2022-11-26 23:31:25 +0530

    adding student class

---
+++ JavaPrograms-lib/src/test/java/JavaPrograms/student.java
@@ -0,0 +1,13 @@
+package JavaPrograms;
+
+public class student {
+	int roll_no;
+	String strStudentName;
+	
+	student(int roll_no,String strStudentName)
+	{
+		this.roll_no = roll_no;
+		this.strStudentName = strStudentName;
+	}
+
+}
